b06ab8233dcfb4ad3ed62fcb7b75a4a60153f9aa
Daan Seegers <daan@raow.nl>
2019-08-02 10:28:07 +0200

    Add container

--- public/index.php
+++ public/index.php
@@ -2,3 +2,9 @@
 
 
 require_once __DIR__. '/../vendor/autoload.php';
+
+$containerBuilder = new \DI\ContainerBuilder();
+$containerBuilder->addDefinitions(__DIR__ . '/../config.php');
+$container = $containerBuilder->build();
+
+echo 'hello 123';
